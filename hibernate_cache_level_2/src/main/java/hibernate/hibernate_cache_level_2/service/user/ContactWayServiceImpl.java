ea654b8b1f30912367455f7d4fc2b0f57f5710b8
xgwfhn@126.com <xgwfhn@126.com>
2016-10-10 12:02:03 +0800

    11

--- hibernate_cache_level_2/src/main/java/hibernate/hibernate_cache_level_2/service/user/ContactWayServiceImpl.java
+++ hibernate_cache_level_2/src/main/java/hibernate/hibernate_cache_level_2/service/user/ContactWayServiceImpl.java
@@ -20,6 +20,7 @@ memCachedService.storeUserAddress("user", "BeiJing");
 name：存储位置。在本来中remote表示使用memcached服务器。
 condition：触发条件，只有满足条件的情况才会加入缓存，默认为空，既表示全部都加入缓存，支持SpEL。
 expire：过期时间，单位为秒。  //这个是别的版本 com.springcache.annotation.Cacheable 才有该属性
+
 最后总结一下：当执行到一个被@Cacheable注解的方法时，Spring首先检查condition条件是否满足，如果不满足，执行方法，返回；
 如果满足，在name所命名的缓存空间中查找使用key存储的对象，如果找到，将找到的结果返回，如果没有找到执行方法，将方法的返回值以key-value对象的方式存入name缓存中，然后方法返回。
  */
@@ -56,6 +57,8 @@ public class ContactWayServiceImpl implements ContactWayService {
 		ContactWay cw=contactWayDao.find(id);
 		return cw;
 	}
+	
+	//@CacheEvict
 	
 	public List<String> findProperty(String hql)throws Exception{
 		 contactWayDao.findProperty(hql);
